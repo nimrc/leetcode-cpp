defb11c6d3f5afb3222b8cc14c60485fd3f2d197
fyibmsd <fyibmsd@gmail.com>
2019-06-28 14:39:20 +0800

    update

--- src/162-find-peak-element.cpp
+++ src/162-find-peak-element.cpp
@@ -23,6 +23,23 @@ public:
 
         return size - 1;
     }
+
+    int findPeakElement2(vector<int> &nums)
+    {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low < high) {
+            int mid1 = low + (high - low) / 2;
+            int mid2 = mid1 + 1;
+
+            if (nums[mid1] > nums[mid2])
+                high = mid1;
+            else
+                low = mid2;
+        }
+        return low;
+    }
 };
 
 int main(int argc, char **argv)
@@ -30,8 +47,10 @@ int main(int argc, char **argv)
     vector<int> nums = {1, 2, 1, 3, 5, 6, 4};
 
     int res = (new Solution)->findPeakElement(nums);
-
     assert_equal(res, 1);
+
+    int res2 = (new Solution)->findPeakElement2(nums);
+    assert_equal(res2, 5);
 
     return 0;
 }
